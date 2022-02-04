e3ff7a3733d2b57a796d87871ceb5aa87d2ad433
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-02-04 06:37:38 +0400

    Template

---
+++ main.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext import commands, tasks
+import os
+from os import listdir
+from os.path import isfile, join
+
+intents = discord.Intents.all()
+intents.presences = True
+intents.members = True
+intents.guilds=True
+intents.all
+
+client = commands.Bot(command_prefix = '.', intents=intents, presences = True, members = True, guilds=True, case_insensitive=True, allowed_mentions = discord.AllowedMentions(everyone=False))
+
+async def update_activity(client):
+    await client.change_presence(activity=discord.Game(f"On {len(client.guilds)} servers! | .help"))
+    print("Updated presence")
+
+@client.event
+async def on_ready():
+    # Setting `Playing ` status
+    print("we have powered on, I an alive.")
+    await update_activity(client)
+    channel = client.get_channel(938986974419767317)
+    await channel.send("Online")
+
+
+
+
+    
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+def start_bot(client):
+    lst = [f for f in listdir("cogs/") if isfile(join("cogs/", f))]
+    no_py = [s.replace('.py', '') for s in lst]
+    startup_extensions = ["cogs." + no_py for no_py in no_py]
+    try:
+        for cogs in startup_extensions:
+            client.load_extension(cogs)  # Startup all cogs
+            print(f"Loaded {cogs}")
+
+        print("\nAll Cogs Loaded\n===============\nLogging into Discord...")
+        client.run(TOKEN) # Token do not change it here. Change it in the .env if you do not have a .env make a file and put DISCORD_TOKEN=Token 
+
+    except Exception as e:
+        print(
+            f"\n###################\nPOSSIBLE FATAL ERROR:\n{e}\nTHIS MEANS THE BOT HAS NOT STARTED CORRECTLY!")
+
+
+
+start_bot(client)
