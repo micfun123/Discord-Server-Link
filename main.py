262450ae2375bf82ddd7e2a38c5edb673dc7d6bf
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-02-05 16:26:16 +0400

    change again

--- main.py
+++ main.py
@@ -14,43 +14,10 @@ intents.guilds=True
 intents.all
 
 
-client = commands.Bot( command_prefix= ;, intents=intents, presences = True, members = True, guilds=True, case_insensitive=True, allowed_mentions = discord.AllowedMentions(everyone=False))
 
 
-@client.event
-async def on_guild_join(guild): #when the bot joins the guild
-    with open('prefixes.json', 'r') as f: #read the prefix.json file
-        prefixes = json.load(f) #load the json file
+client = commands.Bot( command_prefix= ".", intents=intents, presences = True, members = True, guilds=True, case_insensitive=True, allowed_mentions = discord.AllowedMentions(everyone=False))
 
-    prefixes[str(guild.id)] = '.'#default prefix
-
-    with open('prefixes.json', 'w') as f: #write in the prefix.json "message.guild.id": "."
-        json.dump(prefixes, f, indent=4) #the indent is to make everything look a bit neater
-
-@client.event
-async def on_guild_remove(guild): #when the bot is removed from the guild
-    with open('prefixes.json', 'r') as f: #read the file
-        prefixes = json.load(f)
-
-    prefixes.pop(str(guild.id)) #find the guild.id that bot was removed from
-
-    with open('prefixes.json', 'w') as f: #deletes the guild.id as well as its prefix
-        json.dump(prefixes, f, indent=4)
-
-
-@client.command(pass_context=True)
-async def changeprefix(ctx, prefix): #command: bl!changeprefix ...
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes[str(ctx.guild.id)] = prefix
-
-    with open('prefixes.json', 'w') as f: #writes the new prefix into the .json
-        json.dump(prefixes, f, indent=4)
-
-    await ctx.send(f'Prefix changed to: {prefix}') #confirms the prefix it's been changed to
-#next step completely optional: changes bot nickname to also have prefix in the nickname
-    name=f'{prefix}BotBot'
 
 
 async def update_activity(client):
