e12c081f99a5b95137f72235aba1b3ad0d732a83
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-02-05 16:14:08 +0400

    fix

--- main.py
+++ main.py
@@ -14,13 +14,7 @@ intents.guilds=True
 intents.all
 
 
-def get_prefix(client, message): ##first we define get_prefix
-    with open('prefixes.json', 'r') as f: ##we open and read the prefixes.json, assuming it's in the same file
-        prefixes = json.load(f) #load the json as prefixes
-    return prefixes[str(message.guild.id)] #recieve the prefix for the guild id given
-
-
-client = commands.Bot( command_prefix= (get_prefix), intents=intents, presences = True, members = True, guilds=True, case_insensitive=True, allowed_mentions = discord.AllowedMentions(everyone=False))
+client = commands.Bot( command_prefix= ;, intents=intents, presences = True, members = True, guilds=True, case_insensitive=True, allowed_mentions = discord.AllowedMentions(everyone=False))
 
 
 @client.event
